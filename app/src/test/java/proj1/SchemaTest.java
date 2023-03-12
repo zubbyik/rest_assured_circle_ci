14798b6f301f9261ac0c2ec260a1ea59002ae737
Malachy Egbuna <zubbyik@gmail.com>
2023-03-12 17:06:36 +0100

    initial

--- app/src/test/java/proj1/SchemaTest.java
+++ app/src/test/java/proj1/SchemaTest.java
@@ -1,20 +1,20 @@
 package proj1;
 
 import org.junit.jupiter.api.Test;
+
+import java.io.*;
+
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchema;
 import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
 import static io.restassured.RestAssured.get;
 
 
 public class SchemaTest {
-    @Test void schema_testing(){
-        ClassLoader classLoader = getClass().getClassLoader();
-        try {
-            String path = classLoader.getResource("products-schema").getPath();
-            get("https://jsonplaceholder.typicode.com/users").then().body(matchesJsonSchemaInClasspath(path));
-        } catch (NullPointerException e) {
-            System.out.println(e);
-        }
+    @Test void schema_testing() {
+        //get(path).then().body(matchesJsonSchemaInClasspath(path));
+        InputStream path = getClass().getClassLoader().getResourceAsStream("users-schema.json");
+        assert path != null;
+        get("https://jsonplaceholder.typicode.com/users").then().assertThat().body(matchesJsonSchema(path));
 
-        //        get("https://jsonplaceholder.typicode.com/users").then().assertThat().body(matchesJsonSchemaInClasspath(path));
     }
 }
